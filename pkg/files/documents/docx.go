4b7a4775645f0ddd05bb2be6e56271083f0a15b3
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2023-12-23 21:06:54 -0600

    refactor(documents): refactor the objects in the documents since the file interface was modified
    even though those objects are empty

--- pkg/files/documents/docx.go
+++ pkg/files/documents/docx.go
@@ -8,7 +8,7 @@ func (p *Docx) SupportedFormats() map[string][]string {
 	return make(map[string][]string)
 }
 
-func (p *Docx) ConvertTo(format string, fileBytes []byte) ([]byte, error) {
+func (p *Docx) ConvertTo(fileType, subType string, fileBytes []byte) ([]byte, error) {
 	return nil, errors.New("not implemented")
 }
 
